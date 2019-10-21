15fd1c1d16a1d34a077947e51634cf59b780e685
rafern <15365765+rafern@users.noreply.github.com>
2019-10-21 16:52:23 +0100

    fixed indentation

--- facebook-frontend/main.py
+++ facebook-frontend/main.py
@@ -28,22 +28,22 @@ if request.method == 'GET':
 ##################################################################################################################################
 
 # if the request was not get, it must be POST and we can just proceed with sending a message # back to user
-   else:
-        # get whatever message a user sent the bot
-       output = request.get_json()
-       for event in output['entry']:
-          messaging = event['messaging']
-          for message in messaging:
-            if message.get('message'):
-                #Facebook Messenger ID for user so we know where to send response back to
-                recipient_id = message['sender']['id']
-                if message['message'].get('text'):
-                    response_sent_text = get_message()
-                    send_message(recipient_id, response_sent_text)
-                #if user sends us a GIF, photo,video, or any other non-text item
-                if message['message'].get('attachments'):
-                    response_sent_nontext = get_message()
-                    send_message(recipient_id, response_sent_nontext)
+else:
+    # get whatever message a user sent the bot
+    output = request.get_json()
+    for event in output['entry']:
+        messaging = event['messaging']
+        for message in messaging:
+        if message.get('message'):
+            #Facebook Messenger ID for user so we know where to send response back to
+            recipient_id = message['sender']['id']
+            if message['message'].get('text'):
+                response_sent_text = get_message()
+                send_message(recipient_id, response_sent_text)
+            #if user sends us a GIF, photo,video, or any other non-text item
+            if message['message'].get('attachments'):
+                response_sent_nontext = get_message()
+                send_message(recipient_id, response_sent_nontext)
     return "Message Processed"
 
 ##################################################################################################################################
