5c325355bb576823b630ee7bab21fadc45d951b9
rafern <15365765+rafern@users.noreply.github.com>
2019-10-21 15:54:16 +0100

    finished web frontend + rest

--- web-frontend/files/rest-caller.js
+++ web-frontend/files/rest-caller.js
@@ -1,1 +1,36 @@
-// TODO 
+function doQuery() {
+    // Get question
+    queryText = document.getElementById("inputBox").value;
+    
+    // Do REST request
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            // Parse response
+            jsonResponse = JSON.parse(xhttp.responseText);
+            
+            // Add to answer list
+            addAnswer(jsonResponse.response);
+        }
+    };
+    xhttp.open("GET", "/api/query?query=" + encodeURIComponent(queryText), true);
+    xhttp.send();
+}
+
+function addAnswer(text) {
+    // Create answer box
+    newDiv = document.createElement("div");
+    newDiv.className = "answerBox";
+    newDiv.textContent = text;
+    
+    // Add to answer list
+    answerList = document.getElementById("answerList");
+    answerList.insertBefore(newDiv, answerList.firstChild);
+}
+
+function setupAll() {
+    // Setup ask button
+    document.getElementById("askButton").onclick = doQuery;
+}
+
+window.onload = setupAll;
--- web-frontend/queryHandler.py
+++ web-frontend/queryHandler.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
 
-def query(queryString):
-    return {"response": "Placeholder response (not implemented yet)"} # TODO implement
+def query(queries):
+    if "query" not in queries.keys():
+        raise RESTError(0, "Query parameter 'query' missing")
+    
+    return {"response": "Placeholder response (you asked: {})".format(queries["query"][0])} # TODO implement
